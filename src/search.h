fd8d499be9132338570b53b721a8a3d646670b88
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-10-15 08:19:28 -0400

    Refactor time management

--- src/search.h
+++ src/search.h
@@ -1,20 +1,5 @@
 #include "board.h"
-#include <chrono>
-
-using TimePoint = std::chrono::steady_clock::time_point;
-using Duration = std::chrono::milliseconds;
-
-struct SearchLimits
-{
-    int maxDepth;
-    Duration maxTime;
-    struct Clock
-    {
-        Duration time;
-        Duration inc;
-        bool enabled;
-    } clock;
-};
+#include "time_man.h"
 
 struct SearchResult
 {
@@ -32,34 +17,10 @@ public:
 private:
     int search(int depth, int ply);
 
-    // TODO: put time management in separate file
-    void initTimeMan(const SearchLimits& limits)
-    {
-        m_StartTime = std::chrono::steady_clock::now();
-        Duration maxTime = limits.maxTime;
-        if (limits.maxTime <= Duration(0))
-            maxTime = Duration(1000000000);
-        if (limits.clock.enabled)
-            maxTime = std::min(maxTime, limits.clock.time / 20);
-        m_HardLimit = maxTime;
-        std::cout << "info string time " << m_HardLimit.count() << std::endl;
-    }
-
-    Duration elapsed() const
-    {
-        return std::chrono::duration_cast<Duration>(std::chrono::steady_clock::now() - m_StartTime);
-    }
-
-    bool timeUp() const
-    {
-        return elapsed() >= m_HardLimit;
-    }
-
     Board m_Board;
+    TimeMan m_TimeMan;
     Move m_RootBestMove;
 
-    TimePoint m_StartTime;
-    Duration m_HardLimit;
     bool m_ShouldStop;
 
     uint64_t nodes;
---
+++ src/time_man.h
@@ -0,0 +1,34 @@
+#include <chrono>
+
+using TimePoint = std::chrono::steady_clock::time_point;
+using Duration = std::chrono::milliseconds;
+
+struct SearchLimits
+{
+    int maxDepth;
+    Duration maxTime;
+    struct Clock
+    {
+        Duration time;
+        Duration inc;
+        bool enabled;
+    } clock;
+};
+
+class TimeMan
+{
+public:
+    TimeMan();
+
+    void setLimits(const SearchLimits& limits);
+    void startSearch();
+
+    bool shouldStop() const;
+
+    Duration elapsed() const;
+private:
+    SearchLimits m_Limits;
+
+    TimePoint m_StartTime;
+    Duration m_HardLimit;
+};
