e5337479c2d3f1339febae389ca51c2f1cc7b47b
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-10-15 09:23:05 -0400

    Alpha beta pruning Bench: 83730290

--- src/bench.cpp
+++ src/bench.cpp
@@ -34,7 +34,7 @@ const char* benchFens[] = {
 
 void runBench()
 {
-    constexpr int BENCH_DEPTH = 7;
+    constexpr int BENCH_DEPTH = 10;
 
     SearchLimits limits = {};
     limits.maxDepth = BENCH_DEPTH;
