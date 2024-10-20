b02241f59cd69f630285d635efaf9657a7211cc4
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-10-19 22:12:12 -0400

    cleanup Bench: 6155802

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,18 +1,9 @@
 #include "evaluate.h"
+#include "eval_constants.h"
 #include <array>
 
 namespace eval
 {
-
-// a1 b1 c1
-// a2 b2 c2
-// a3 b3 c3
-constexpr std::array<int, 9> psqt =
-{
-    21, 15, 22,
-    14, 28, 15,
-    22, 15, 21
-};
 
 template<Color us, bool Trace>
 int evaluatePsqt(const Board& board)
